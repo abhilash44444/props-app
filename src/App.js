e4e9dc5c11e7cfa9aad33971cbcdc9aedb9193ee
abhilash44444 <leariningcourses@gmail.com>
2024-01-27 16:45:06 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './style.css';
+import Nav from './Nav';
 
 export default function App() {
   return (
     <div>
+      <Nav/>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
---
+++ src/Nav.js
@@ -0,0 +1,22 @@
+
+ const Nav=()=>{
+
+     return (
+
+            <div>
+
+             <div>
+                LOGO
+             </div>
+        <div>
+            <ul>Home</ul>
+            <ul>Service</ul>
+            <ul>Contact</ul>
+            <ul>Login</ul>
+        </div>
+
+            </div>
+     )
+ }
+
+ export default Nav;
